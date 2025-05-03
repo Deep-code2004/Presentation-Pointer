80b2b6f2ee43cdd0205ff125b69a777bfd3c4949
DEEP KHATRI <deepkhatri050@gmail.com>
2025-05-03 23:54:12 +0530

    Here You Can See how i convert img numerical number to image display ...

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ cap.set(4, 480)  # Set height
 pathImages = sorted(os.listdir(folderPath), key=len)
 print(pathImages)
 
+imgNumber = 0
+
 while True:
   success, img =cap.read()
-  
-  
+  pathFullImage = os.path.join(folderPath,pathImages[imgNumber])
+  imgCurrent = cv2.imread(pathFullImage)
   
   cv2.imshow("Image", img)
+  cv2.imshow("Presentation", imgCurrent)
   key = cv2.waitKey(1)
   if key == ord('q'):
     break
