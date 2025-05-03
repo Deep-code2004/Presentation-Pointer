c6f76a5de829e382144681f1adee201dddf1712f
DEEP KHATRI <deepkhatri050@gmail.com>
2025-05-04 00:28:02 +0530

    After Display Slides There Is A Issue Which Are Showing Big Size Of Display Slides  So I solved This Issue By Resize the image You Can See ON line 22 & 23

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ height = 720
 folderPath = "Slides"
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)  # Set width
-cap.set(4, 480)  # Set height
+cap.set(3, width)  # Set width
+cap.set(4, height)  # Set height
 
 pathImages = sorted(os.listdir(folderPath), key=len)
 print(pathImages)
@@ -14,12 +14,16 @@ print(pathImages)
 imgNumber = 0
 
 while True:
-  success, img =cap.read()
-  pathFullImage = os.path.join(folderPath,pathImages[imgNumber])
-  imgCurrent = cv2.imread(pathFullImage)
-  
-  cv2.imshow("Image", img)
-  cv2.imshow("Presentation", imgCurrent)
-  key = cv2.waitKey(1)
-  if key == ord('q'):
-    break
+    success, img = cap.read()
+    pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
+    imgCurrent = cv2.imread(pathFullImage)
+    
+    # Resize the images to the desired width and height
+    img = cv2.resize(img, (width, height))
+    imgCurrent = cv2.resize(imgCurrent, (width, height))
+    
+    cv2.imshow("Image", img)
+    cv2.imshow("Presentation", imgCurrent)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
