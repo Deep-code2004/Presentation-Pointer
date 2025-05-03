e8beca18652e2134dcf24147d699512768977d3e
DEEP KHATRI <deepkhatri050@gmail.com>
2025-05-03 21:25:36 +0530

    Here I get a bug Bsically I have 15 slides And concept Say's That Listing Squence wise mean 1,2,3 ....9 so It's gives 1,10,11,12,13,14,15,2,3,...9 so this Bug I have To Solve It By Sort Method You Can Check On Line 11

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # Set width
 cap.set(4, 480)  # Set height
 
-pathImages = os.listdir(folderPath)
+pathImages = sorted(os.listdir(folderPath), key=len)
 print(pathImages)
 
 while True:
